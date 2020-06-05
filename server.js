6b9d8bacf09942476e22f60a16af0b982484f93a
Charlie Thao <cthao309@gmail.com>
2020-06-05 01:06:52 -0700

    added redirect on server.js to re-route the endpoint to micr-services

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const PORT = process.env.PROXY_PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.listen(PORT);
+app.get('/looks/:id', (req, res) => {
+  axios.get(`http://52.53.187.183:5000/looks/${req.params.id}`)
+  .then(({data}) => {
+    res.status(200).send(data)
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
+});
+
+app.get('/relatedProduct/:id', (req, res) => {
+  axios.get(`http://52.53.187.183:5000/relatedProduct/${req.params.id}`)
+  .then(({data}) => {
+    res.status(200).send(data)
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
+});
+
+app.listen(PORT, () => console.log(`proxy server is listening on port: http://localhost:${PORT}`));
